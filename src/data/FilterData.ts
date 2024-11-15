44ad54d2cef074a09ae9952c63e75e173058c6a3
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-15 13:07:01 +0100

    Array of Data created and updated

--- src/data/FilterData.ts
+++ src/data/FilterData.ts
@@ -1,21 +1,56 @@
 export const filterData = [
-{
-    name: "nature",
-    src: "https://images.pexels.com/photos/933054/pexels-photo-933054.jpeg?auto=compress&cs=tinysrgb&w=800",
-    title: "Mountains",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-},
-
-{
-    name: "cars",
-    src: "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=800",
-    title: "BMW",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-},
-{
-name: "people",
-src: "https://images.pexels.com/photos/428338/pexels-photo-428338.jpeg?auto=compress&cs=tinysrgb&w=800",
-title: "Modeller",
-text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-},
+    {
+        name: "nature",
+        src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/magical-spring-forest-scenery-during-morning-breeze-free-photo.jpg?w=600&quality=80",
+        title: "Nature is Beautiful 🍄‍🟫",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "cars",
+        src: "https://img.freepik.com/premium-photo/lamborghini-rain-wallpapers_469760-5328.jpg",
+        title: "Lamborghini 🐂",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "people",
+        src: "https://media.istockphoto.com/id/1460179345/photo/teamwork-collaboration-and-planning-black-women-with-documents-paperwork-or-design-strategy.jpg?s=612x612&w=0&k=20&c=1OHBYpzEMpO8NlqGmL8mHW9XAjTV0t-froey2ZlqhYA=",
+        title: "Teamwork is Key ✌🏿",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "nature",
+        src: "https://images.squarespace-cdn.com/content/v1/5e608f8a34e0e804f1e7da3b/896d097b-b89f-4811-a8e0-43a2d419c7a1/DSC_6772.JPG",
+        title: "Get Back to Nature",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "cars",
+        src: "https://hips.hearstapps.com/hmg-prod/images/2025-toyota-camry-xse-awd-123-66993cc94cc40.jpg?crop=0.580xw:0.489xh;0.137xw,0.397xh&resize=700:*",
+        title: "Toyota",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "people",
+        src: "https://media.istockphoto.com/id/1342849839/photo/beautiful-afro-woman-with-pigtails-and-stylish-clothes.jpg?s=612x612&w=0&k=20&c=tR_IMMATG4jHJTuGgwQ-hjuY7mte7fEtEvYAefjjbtg=",
+        title: "Party Time!!",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "nature",
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThxFeq0ZlJFcRCponlz9TGuSk1p_hbNkDsJw&s",
+        title: "Forest",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "cars",
+        src: "https://d3s8goeblmpptu.cloudfront.net/mrp/toyota/2023/bz4x/2023-toyota-bz4x_landing-6.jpg",
+        title: "Toyota",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "people",
+        src: "https://media.istockphoto.com/id/1362265241/photo/business-people-in-the-office.jpg?s=612x612&w=0&k=20&c=b7HXF98Egp0ls04tsRz45-5NWYTwpCPMUswYTdkecD8=",
+        title: "Work 🧑🏿‍💻",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
 ]
