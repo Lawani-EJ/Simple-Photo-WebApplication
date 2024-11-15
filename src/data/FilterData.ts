7b6a5faf7d5c9aa1e4e4552d7667cc1925903132
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-15 13:06:01 +0100

    Array of Data created

---
+++ src/data/FilterData.ts
@@ -0,0 +1,21 @@
+export const filterData = [
+{
+    name: "nature",
+    src: "https://images.pexels.com/photos/933054/pexels-photo-933054.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Mountains",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+},
+
+{
+    name: "cars",
+    src: "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "BMW",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+},
+{
+name: "people",
+src: "https://images.pexels.com/photos/428338/pexels-photo-428338.jpeg?auto=compress&cs=tinysrgb&w=800",
+title: "Modeller",
+text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+},
+]
